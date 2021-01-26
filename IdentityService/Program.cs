d778d944253c7647e382a8c5aeea3710fad4b2c4
Peter Björck <pb@psksyd.com>
2021-01-26 09:47:31 +0100

    Exercise 3.2

--- IdentityService/Program.cs
+++ IdentityService/Program.cs
@@ -23,16 +23,55 @@ namespace IdentityService
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    if (context.HostingEnvironment.EnvironmentName != "Offline")
-                        config.AddAzureKeyVaultSupport();
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            Console.WriteLine($"ASPNETCORE_ENVIRONMENT='{env}'");
+
+            switch (env)
+            {
+                case "Production":
+                    Console.WriteLine("Configuring Kestrel for production");
+
+                    //Use Azure KeyVault for config + Https certificate in key vault
+                    return Host.CreateDefaultBuilder(args)
+                        .ConfigureAppConfiguration(config =>
+                        {
+                            config.AddAzureKeyVaultSupport();
+                        })
+                        .ConfigureWebHostDefaults(webBuilder =>
+                        {
+                            webBuilder.AddHttpsSupport(certName: "webapi-certificate");
+
+                            webBuilder.UseStartup<Startup>()
+                                .UseUrls("http://*:80;https://*:443");
+                        });
+                case "Offline":
+                    Console.WriteLine("Configuring Kestrel for offline");
+
+                    //Use local configuration and the built in localhost developer certificate
+                    return Host.CreateDefaultBuilder(args)
+                        .ConfigureWebHostDefaults(webBuilder =>
+                        {
+                            webBuilder.UseStartup<Startup>();
+                        });
+
+                default:
+                    //Development
+                    Console.WriteLine("Configuring Kestrel for development");
+
+                    //Use Azure Key Vault for config and the built in localhost developer certificate
+                    return Host.CreateDefaultBuilder(args)
+                        .ConfigureAppConfiguration(config =>
+                        {
+                            config.AddAzureKeyVaultSupport();
+                        })
+                        .ConfigureWebHostDefaults(webBuilder =>
+                        {
+                            webBuilder.UseStartup<Startup>();
+                        });
+            }
+        }
+
     }
 }
---
+++ Infrastructure/HttpsExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Security.KeyVault.Certificates;
+using Microsoft.Extensions.Configuration;
+using Azure.Identity;
+using Microsoft.AspNetCore.Hosting;
+using System.Net;
+using System.Security.Authentication;
+using System.Net.Security;
+
+namespace Infrastructure
+{
+    public static class HttpsExtensions
+    {
+        /// <summary>
+        /// Adds HTTPS support to Kestrel using a certificate in Azure Key Vault
+        /// </summary>
+        /// <param name="webBuilder"></param>
+        /// <param name="certName">Name of the certificate to load</param>
+        /// <returns></returns>
+        public static IWebHostBuilder AddHttpsSupport(this IWebHostBuilder webBuilder, string certName)
+        {
+            Console.WriteLine($"Adding HTTPS Support with the certificate named '{certName}' in Azure Key Vault");
+            webBuilder.UseKestrel((context, serverOptions) =>
+               {
+                   serverOptions.Listen(IPAddress.Any, 80);
+
+                   serverOptions.Listen(IPAddress.Any, 443,
+                       options =>
+                       {
+                           var cert = KeyVaultExtensions.LoadCertificate(context.Configuration, certName);
+                           options.UseHttps(serverCertificate: cert, configureOptions: httpsOptions =>
+                           {
+
+                               //Add HTTPS configuration here
+
+                           });
+                       });
+               });
+
+            return webBuilder;
+        }
+    }
+}
